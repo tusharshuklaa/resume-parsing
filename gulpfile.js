cf5b9ca66573e0155546690ba26dd30535f82a59
Tushar Shukla <tusharshuklaa@gmail.com>
2018-11-25 21:26:29 +0530

    Restructured project

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,78 @@
-const { 
+// Requiring modules
+const {
   parallel,
+  series,
   src,
-  dest
+  dest,
+  watch
 } = require('gulp'),
-sass = require('gulp-sass');
-sass.compiler = require('node-sass');
+  sass = require('gulp-sass'),
+  concat = require('gulp-concat'),
+  sourcemaps = require('gulp-sourcemaps'),
+  // rename = require("gulp-rename"),
+  uglify = require('gulp-uglify'),
+  pump = require('pump'),
+  scssUrl = "src/scss",
+  vendorCss = "src/vendor/css",
+  vendorJs = "src/vendor/js",
+  jsUrl = "src/js",
+  destCss = "dest/assets/css",
+  destJs = "dest/assets/js",
+  srcFonts = "src/vendor/fonts",
+  destFonts = "dest/assets/fonts";
 
-const sassify = function() {
-  return src('src/scss/base.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(dest('css'));
+  // Gulp Tasks
+const handleVendorCss = function(cb) {
+  pump([
+      src([vendorCss + '/bootstrap.min.css', vendorCss + '/font-awesome.min.css']),
+      sourcemaps.init(),
+      concat({
+        path: 'vendor.min.css'
+      }),
+      sourcemaps.write(),
+      dest(destCss)
+    ],
+    cb
+  );
 },
-copyJs = function() {
-  return src('src/js/main.js')
-    .pipe(dest('js'));
-};
+sassify = function (cb) {
+  pump([
+    src(scssUrl + '/base.scss'),
+    sass().on('error', sass.logError),
+    dest(destCss)
+  ], cb);
+},
+watchSass = function () {
+  return watch(scssUrl + '/**/*.scss', series(sassify));
+},
+watchJs = function () {
+  return watch(jsUrl + '/**/*.js', series(copyJs));
+},
+handleVendorJs = function(cb) {
+  pump([
+    src([vendorJs + '/jquery.min.js', vendorJs + '/bootstrap.min.js']),
+    sourcemaps.init(),
+    concat({
+      path: 'vendor.min.js'
+    }),
+    uglify(),
+    sourcemaps.write(),
+    dest(destJs)
+  ], cb);
+},
+copyJs = function (cb) {
+  pump([
+    src(jsUrl + '/main.js'),
+    dest(destJs)
+  ], cb);
+},
+copyFonts = function (cb) {
+  pump([
+    src(srcFonts + '/**/*.*'),
+    dest(destFonts)
+  ], cb);
+},
+handleAll = parallel(handleVendorCss, sassify, handleVendorJs, copyJs, copyFonts);
 
-
-exports.default = parallel(sassify, copyJs);
+// Default build task
+exports.default = series(handleAll, parallel(watchSass, watchJs));
